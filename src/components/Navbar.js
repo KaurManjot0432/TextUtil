ba042c09593c2c414e06747c6ef8372138028756
KaurManjot0432 <0432manjot@gmail.com>
2021-09-13 15:56:41 +0530

    added tolowerCase

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Navbar(props) {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">{props.title}</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,7 +23,7 @@ export default function Navbar(props) {
             </ul>
             <form class="d-flex">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button class="btn btn-outline-success" type="submit">Search</button>
+              <button class="btn btn-primary" type="submit">Search</button>
             </form>
           </div>
         </div>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -5,21 +5,33 @@ export default function Textform(props) {
     // text = "sdkafafaj"//wrong way to change the state
     // setText("sfdagsd");//right wat to change the state
     const handleUPClick = ()=>{
-        console.log("Uppercase was clicked");
         let uppercaseText = text.toUpperCase();
         setText(uppercaseText);
     }
+    const handleLCClick = ()=>{
+        let uppercaseText = text.toLowerCase();
+        setText(uppercaseText);
+    }
     const handleOnChange = (event)=>{
-        console.log("on change");
         setText(event.target.value);
     }
     return (
-        <div>
+        <>
+        <div class="container">
             <h2>{props.heading}</h2>
             <div class="mb-3">
                 <textarea class="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
             </div>
-            <button class="btn btn-primary" onClick={handleUPClick}>convert to UpperCase</button>
+            <button class="btn btn-primary mx-1" onClick={handleUPClick}>convert to UpperCase</button>
+            <button class="btn btn-primary mx-1" onClick={handleLCClick}>convert to UpperCase</button>
         </div>
+        <div class="container my-4">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} min read</p>
+            <h3>Preview</h3>
+            <p>{text}</p>
+            </div>
+        </>
     )
 }
